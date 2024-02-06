ebbfe85532aa8ac9b1e585c676e1d46a69ecf8c1
Kwiper <jonny.tibboh@gmail.com>
2024-02-06 15:25:35 -0800

    Bonus video content

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,12 +13,18 @@ public class Enemy : MonoBehaviour
 
     Score score; // Score
 
+    bool isDead;
+
     // Start is called before the first frame update
     void Start()
     {
+        isDead = false;
         player = FindObjectOfType<FPSController>().transform; // Find the player in the scene and assign transform to player variable
         agent = GetComponent<NavMeshAgent>(); // Get navmeshagent component
         score = FindObjectOfType<Score>(); // Find the score in scene
+
+        SetRigidBodyState(true);
+        SetColliderState(false);
     }
 
     // Update is called once per frame
@@ -39,16 +45,45 @@ public class Enemy : MonoBehaviour
 
     void Die() // Die
     {
+        isDead = true;
+        agent.speed = 0f;
+        GetComponent<Animator>().enabled = false;
+        SetRigidBodyState(false);
+        SetColliderState(true);
         score.AddScore(100);
-        Destroy(gameObject);
+        Destroy(gameObject, 5f);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !isDead)
         {
             collision.gameObject.GetComponent<FPSController>().TakeDamage(damage);
         }
+    }
+
+    void SetRigidBodyState(bool state)
+    {
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = state;
+        }
+
+        GetComponent<Rigidbody>().isKinematic = !state;
+    }
+
+    void SetColliderState(bool state)
+    {
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = state;
+        }
+
+        GetComponent<Collider>().enabled = !state;
     }
 
 }
